fffc13c85fd679fce7510fcd1121d9db21151f66
Juan Pablo <juanpo996@gmail.com>
2019-08-17 19:37:03 -0700

    finalizacion de manejo de string

---
+++ String/separarCadenas.py
@@ -0,0 +1,11 @@
+def run():
+    myString ="Python"
+    print(myString[1])
+    print(myString[1:])
+    print(myString[1:3])
+    print(myString[1:6:2])
+    print(myString[::-1])
+    print(myString[0:6:2]) 
+
+if __name__ == "__main__":
+    run()
